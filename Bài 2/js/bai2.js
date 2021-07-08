32e3edea5b5ebf653d607347486fd0dce5b10a40
HuyNguyen <huynguyen2509@gmail.com>
2021-07-08 21:19:58 +0700

    done bai 2

--- Bài 2/js/bai2.js
+++ Bài 2/js/bai2.js
@@ -1,13 +1,31 @@
 
-document.getElementById("XinChao").addEventListener('click', xinchao);
 function xinchao() {
-    var user = document.getElementById('user');
+    var text = "";
+    var name = prompt("Cho hỏi ai đang sử dụng máy tính");
 
-    switch (user) {
-        case "Bo": {
-            console.log("Vợ Chào Chồng");
-        }
+    switch (name) {
+        case "B":
+            text = "Robot xin hỏi tối nay Ông Chủ bắt kèo Euro nào?";
+            break;
+        case "M":
+            text = "Robot xin hỏi Bà Chủ hôm nay nấu món gì?";
+            break;
+        case "Bro":
+            text = "Robot hỏi Cậu chủ hôm nay đi làm cảm thấy thế nào?";
+            break;
+        case "Sis":
+            text = "Robot hỏi Tiểu Thư có đi chơi trong mùa dịch này không?";
+            break;
         default:
-            console.log("None0");
+            text = ">>> Người lạ xâm nhập máy tính <<<";
+            break;
     }
+
+    var divHT = document.getElementById('divHienThi');
+    var pEL = document.createElement('p');
+    divHT.appendChild(pEL);
+    pEL.innerHTML = text;
+
+    divHT.style.display = "block";
 }
+
